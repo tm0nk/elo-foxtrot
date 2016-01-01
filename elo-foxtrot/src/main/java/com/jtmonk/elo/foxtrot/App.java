12a3a2f269c21e46b14c7982bb8ff6f205dd8181
Tom Monk <tom@jtmonk.com>
2016-01-01 12:19:38 -0800

    Initial commit of maven artifact

---
+++ elo-foxtrot/src/main/java/com/jtmonk/elo/foxtrot/App.java
@@ -0,0 +1,13 @@
+package com.jtmonk.elo.foxtrot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
